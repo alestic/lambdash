55e038275ba2f722e7c9b8cc90e297459ab61888
Eric Hammond <ehammond@thinksome.com>
2015-06-18 04:20:33 -0700

    Simplify Lambda function code even more. Exit lambdash program with correct exit code from the remote command.

--- index.js
+++ index.js
@@ -3,28 +3,18 @@
 //
 // See also: https://alestic.com/2014/11/aws-lambda-shell/
 //
-var AWS   = require('aws-sdk');
+var AWS = require('aws-sdk');
 var exec = require('child_process').exec;
 var MAX_OUTPUT = 1024 * 1024 * 1024; // 1 GB
-var _command = function(command, callback) {
-    var child = exec(command, {encoding: 'binary', maxBuffer: MAX_OUTPUT},
+exports.handler = function(event, context) {
+    var child = exec(event.command, {encoding: 'binary', maxBuffer: MAX_OUTPUT},
         function (error, stdout, stderr) {
             var result = {
                 "stdout": new Buffer(stdout, 'binary').toString('base64'),
                 "stderr": new Buffer(stderr, 'binary').toString('base64'),
-                "code": error
+                "error": error
             };
-            callback(null, result);
-        }
-    );
-}
-exports.handler = function(event, context) {
-    var command = event.command;
-    _command(command, function(err, result) {
-        if (err) {
-            context.fail(err);
-        } else {
             context.succeed(result);
         }
-    });
+    );
 }
